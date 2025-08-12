c2f4d2d6d8b8214dc95f1c5dc40e42f10b696c0f
wak96 <walidayaankawsar@gmail.com>
2025-08-12 12:34:10 -0700

    added project section

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('pages/about.html', views.about, name='about'),
     path('pages/blog.html', views.blog, name='blog'),
+    path('pages/project.html', views.project, name='project'),
 ]
