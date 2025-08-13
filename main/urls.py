52a5c1c4c68cb8bcadc020e8941ece9c33c95849
wak96 <walidayaankawsar@gmail.com>
2025-08-13 05:33:40 -0700

    urls added with publications

--- main/urls.py
+++ main/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('pages/about.html', views.about, name='about'),
     path('pages/blog.html', views.blog, name='blog'),
     path('pages/project.html', views.project, name='project'),
+    path('pages/publications.html', views.publications, name='publications'),
 ]
