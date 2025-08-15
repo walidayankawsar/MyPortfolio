0b562c852b36c7ac994398699c8981ff97e934cf
walidayankawsar <walidayankawsar@gmail.com>
2025-08-15 07:46:24 -0700

    profile pic add

--- main/views.py
+++ main/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from . models import Profile
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    profile = Profile.objects.all()
+    return render(request, 'index.html', {'pic': profile})
 
 def main(request):
     return render(request, 'index.html')
