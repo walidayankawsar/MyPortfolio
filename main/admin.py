c66079fe6b923cb44e444ab2d37aab8cc574f502
wak96 <walidayaankawsar@gmail.com>
2025-08-18 09:14:30 -0700

    reg another class

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from . models import Profile, Tag, Project
+from . models import Profile, Project_Tag, Project
 
 # Register your models here.
 admin.site.register(Profile)
-admin.site.register(Tag)
+admin.site.register(Project_Tag)
 admin.site.register(Project)
