f21f00afd2228c1cbee5efea6c0aed276cfc6453
walidayankawsar <walidayankawsar@gmail.com>
2025-08-19 02:21:46 -0700

    updated

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -122,7 +122,7 @@ STATICFILES_DIRS = [
 
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR/'Media'
+MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
