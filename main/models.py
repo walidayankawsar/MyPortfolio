586120d5026bda4d257b952668a9e9847758d58e
walidayankawsar <walidayankawsar@gmail.com>
2025-08-19 00:55:46 -0700

    delete database and added tags and resolve some bugs

--- main/models.py
+++ main/models.py
@@ -2,23 +2,24 @@ from django.db import models
 
 # Create your models here.
 class Profile(models.Model):
-    picture = models.ImageField(upload_to='profile/')
-    graduate = models.CharField(max_length=100)
+    picture = models.ImageField(upload_to='profile/', blank=True, null=True)
+    graduate = models.CharField(max_length=100, blank=True, null=True)
     cv = models.FileField(upload_to='cv/', blank=True, null=True)
-    description = models.TextField()
-    work = models.CharField(max_length=100)
-    work_experience = models.TextField()
-    work_date = models.DateField()
-    education = models.CharField(max_length=100)
-    education_description = models.TextField()
-    education_date = models.DateField()
+    description = models.TextField(blank=True, null=True)
+    work = models.CharField(max_length=100, blank=True, null=True)
+    work_experience = models.TextField(blank=True, null=True)
+    work_date = models.DateField(blank=True, null=True)
+    education = models.CharField(max_length=100, blank=True, null=True)
+    education_description = models.TextField(blank=True, null=True)
+    education_date = models.DateField(blank=True, null=True)
 
 class Project_Tag(models.Model):
-    Tag_name = models.CharField(max_length=100)
+    Tag_name = models.CharField(max_length=100, blank=True, null=True)
 
 class Project(models.Model):
-    project_name = models.CharField(max_length=100)
-    tags = models.ManyToManyField(Project_Tag, related_name="posts")
-    description = models.TextField()
-    github_link = models.URLField()
-    website_link = models.URLField()
+    project_name = models.CharField(max_length=100, blank=True, null=True)
+    tags = models.ManyToManyField(Project_Tag, related_name="posts", blank=True, null=True)
+    short_description = models.TextField(max_length=500,blank=True, null=True)
+    ful_description = models.TextField(blank=True, null=True)
+    github_link = models.URLField(blank=True, null=True)
+    website_link = models.URLField(blank=True, null=True)
