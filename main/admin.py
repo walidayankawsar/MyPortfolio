23601ee1fa737c6b9164ff4a7f3dcf2b85d3d917
walidayankawsar <walidayankawsar@gmail.com>
2025-08-19 02:04:23 -0700

    added class publications and fixed some bugs

--- main/admin.py
+++ main/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from . models import Profile, Project_Tag, Project
+from . models import Profile, Project_Tag, Project, Publications
 
 # Register your models here.
 admin.site.register(Profile)
 admin.site.register(Project_Tag)
 admin.site.register(Project)
+admin.site.register(Publications)
