80c6510b9ea64471f9902724f980d16d6f4ceb69
wak96 <walidayaankawsar@gmail.com>
2025-08-24 07:27:29 -0700

    path updated and fixed some error

--- main/urls.py
+++ main/urls.py
@@ -7,12 +7,11 @@ urlpatterns = [
     path('index.html', views.main, name='main'),
     path('pages/about.html', views.about, name='about'),
     path('pages/blog.html', views.blog, name='blog'),
-    path('pages/<int:post_id>/', views.blogdetails, name='blogdetails'),
     path('pages/project.html', views.project, name='project'),
     path('pages/<int:postid>/', views.viewProject, name='viewProject'),
     path('pages/publications.html', views.publications, name='publications'),
     path('pages/contact.html', views.contact, name='contact'),
-    
+
     path('pages/index.html', views.main, name='main'),
     path('pages/pages/publications.html', views.publications, name='publications'),
     path('pages/pages/blog.html', views.blog, name='blog'),
@@ -24,6 +23,7 @@ urlpatterns = [
     path('pages/<int:post_id>/publications.html', views.publications_2, name='publications'),
     path('pages/<int:post_id>/contact.html', views.contact_2, name='contact'),
     path('pages/<int:post_id>/blog.html', views.blog_2, name='blog'),
-    path('pages/<int:postid>/project.html', views.project_2, name='project'),
+    path('pages/<int:post_id>/', views.blogdetails, name='blogdetails'),
+    path('pages/<int:post_id>/project.html', views.project_2, name='project'),
 
 ]
