542111c14e9d5a6b07c9cc82c59fe2693086f5d9
walidayankawsar <walidayankawsar@gmail.com>
2025-09-06 00:33:05 -0700

    added env

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -19,11 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@sgr_56le%z@2@ka8e!2a=d%)aw9ao#ijak1sl2rpft4kuzn7+'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -143,12 +139,24 @@ MESSAGE_TAGS = {
 
 }
 
+import environ
+
+env =environ.Env()
+env.read_env(BASE_DIR / ".env")
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = env("SECRET_KEY", default="fallback-secret-key")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = env("DEBUG")
+
 
 # send message to mail
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'walidayaankawsar@gmail.com'
-EMAIL_HOST_PASSWORD = 'gavl rzmg dyex dhss'
-DEFAULT_FROM_EMAIL =EMAIL_HOST_USER
+EMAIL_HOST = env("EMAIL_HOST")
+EMAIL_PORT = env("EMAIL_PORT")
+EMAIL_USE_TLS = env("EMAIL_USE_TLS")
+EMAIL_HOST_USER = env("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
