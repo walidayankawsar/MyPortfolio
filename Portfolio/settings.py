ea9b759a1b584f7ee78c658bd52b81b530698a76
walidayankawsar <walidayankawsar@gmail.com>
2025-09-10 05:30:40 -0700

    whitenose and allowed all host

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 
-ALLOWED_HOSTS = ['walidayankawsar.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -44,9 +44,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # only for render
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 ROOT_URLCONF = 'Portfolio.urls'
 
