97438bb85962dcaa9b68664bd759e5ec1a69651a
Walid Ayaan Kawsar <walidayaankawsar@gmail.com>
2025-09-20 02:41:14 -0900

    removed white nose

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -53,11 +53,9 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', 
+    'django.middleware.clickjacking.XFrameOptionsMiddleware', 
 ]
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 ROOT_URLCONF = 'Portfolio.urls'
