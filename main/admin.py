9a244c4638b01f768221450dc2bdd8284ed3823d
walidayankawsar <walidayankawsar@gmail.com>
2025-11-16 03:11:49 -0800

    Education are registered

--- main/admin.py
+++ main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from . models import Profile, Tags, Project, Publications, Contact, Blog, skill
+from . models import Profile, Tags, Project, Publications, Contact, Blog, skill, Education
 
 # Register your models here.
 admin.site.register(Profile)
@@ -9,3 +9,4 @@ admin.site.register(Publications)
 admin.site.register(Blog)
 admin.site.register(Contact)
 admin.site.register(skill)
+admin.site.register(Education)
