fd9bb808a3038224324b737e24ed573fc2b3e144
walidayankawsar <walidayankawsar@gmail.com>
2025-11-20 00:09:05 -0800

    add dynamic url

--- main/urls.py
+++ main/urls.py
@@ -4,37 +4,17 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('index', views.main, name='main'),
-    path('pages/about', views.about, name='about'),
-    path('pages/blog', views.blog, name='blog'),
-    path('pages/project', views.project, name='project'),
-    path('pages/publications', views.publications, name='publications'),
-    path('pages/contact', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('blog/', views.blog, name='blog'),
+    path('project/', views.project, name='project'),
+    path('publications/', views.publications, name='publications'),
+    path('contact/', views.contact, name='contact'),
 
-    path('pages/index/', views.main, name='main'),
-    path('pages/pages/publications', views.publications, name='publications'),
-    path('pages/pages/blog', views.blog, name='blog'),
-    path('pages/pages/contact', views.contact, name='contact'),
-    path('pages/pages/project', views.project, name='project'),
-    path('pages/pages/about', views.about, name='about'),
+    path('blog/<int:post_id>/', views.blogdetails, name='blogdetails'),
+    path('project/<int:post_id>/', views.viewProject, name='viewProject'),
 
-    path('pages/<int:post_id>/about', views.about_2, name='about'),
-    path('pages/<int:post_id>/publications', views.publications_2, name='publications'),
-    path('pages/<int:post_id>/contact', views.contact_2, name='contact'),
-    path('pages/<int:post_id>/blog', views.blog_2, name='blog'),
-    path('pages/<int:post_id>/', views.blogdetails, name='blogdetails'),
-    path('pages/<int:post_id>/project', views.project_2, name='project'),
-    
-    path('pages/project/<int:post_id>/', views.viewProject, name='viewProject'),
-    path('pages/project/index', views.main, name='main'),
-    path('pages/project/<int:post_id>/about', views.about_2, name='about'),
-    path('pages/project/<int:post_id>/publications', views.publications_2, name='publications'),
-    path('pages/project/<int:post_id>/contact', views.contact_2, name='contact'),
-    path('pages/project/<int:post_id>/blog', views.blog_2, name='blog'),
-    path('pages/project/<int:post_id>/project', views.project_2, name='project'),
-
-    path('pages/search/', views.search, name='search'),
-    path('pages/project_search', views.project_search, name='project_search'),
+    path('search/', views.search, name='search'),
+    path('project_search/', views.project_search, name='project_search'),
 
 
 ]
