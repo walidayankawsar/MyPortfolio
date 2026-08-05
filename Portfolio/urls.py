1895b2423c6c8d16cd4ab5c96c0f874f8a2a3ced
walidayankawsar <walidayankawsar@gmail.com>
2026-08-05 15:56:14 +0600

    Fix admin route static serve issue for production

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -1,19 +1,3 @@
-"""
-URL configuration for Portfolio project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
@@ -21,10 +5,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # আপনার অন্যান্য অ্যাপের URL pattern এখানে থাকবে (যেমন path('', include('main.urls')),)
+    path('', include('main.urls')),  # আপনার মূল অ্যাপের নাম অনুযায়ী দেবেন
 ]
 
-# শুধুমাত্র Development Mode-এ Media এবং Static ফাইল সার্ভ করার জন্য
+# শুধুমাত্র Local machine-এ DEBUG=True থাকলে media/static সার্ভ হবে
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
