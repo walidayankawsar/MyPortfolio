26347a54fe812c0d4b82a04ebc880e33660e398f
walidayankawsar <walidayankawsar@gmail.com>
2026-08-05 15:53:56 +0600

    updated

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -15,16 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.urls import include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls')),
-    path('pages/', include('main.urls')),
+    # আপনার অন্যান্য অ্যাপের URL pattern এখানে থাকবে (যেমন path('', include('main.urls')),)
 ]
 
-
-urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# শুধুমাত্র Development Mode-এ Media এবং Static ফাইল সার্ভ করার জন্য
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
