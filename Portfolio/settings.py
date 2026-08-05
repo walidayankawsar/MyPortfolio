e3d054384ee1a9424f62a5af5a8ff4e6d3dbb8cb
walidayankawsar <walidayankawsar@gmail.com>
2026-08-05 16:05:38 +0600

    updated

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -183,7 +183,7 @@ env.read_env(BASE_DIR / ".env")
 SECRET_KEY = env("SECRET_KEY", default="fallback-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env("DEBUG")
+DEBUG = env.bool('DEBUG', default=False)
 
 
 # send message to mail
