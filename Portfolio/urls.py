c79cb2c023a50fc46039f0fcc9bbe904c3b752cd
Walid Ayan Kawsar <walidayankawsar@gmail.com>
2026-08-06 12:29:58 +0600

    Update urls.py

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
-    path('pages/', include('main.urls')),
+    #path('pages/', include('main.urls')),
 ]
 
 # শুধুমাত্র Development/Local-এর জন্য static/media serve করুন
