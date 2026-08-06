35940a7810cdac5500ebf0dddf079c057089f9a3
Walid Ayan Kawsar <walidayankawsar@gmail.com>
2026-08-06 12:33:40 +0600

    Update settings.py

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -157,7 +157,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Media files (User uploaded files)
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Cloudinary Credentials Setup
 # os.environ.get() ব্যবহার করলে env Variable না থাকলেও Server crash করবে না
@@ -167,7 +166,10 @@ CLOUDINARY_STORAGE = {
     'API_SECRET': os.environ.get('CLOUD_API_SECRET'),
 }
 
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+if ENVIRONMENT == 'production' or POSTGRES_LOCALLY == True:
+    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+else:
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Django 4.2+ / Django 5.x-এর জন্য নতুন Storage Configuration
 STORAGES = {
