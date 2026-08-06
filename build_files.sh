15cb304e2c384c7c36330785507290c0fbc87bfb
walidayankawsar <walidayankawsar@gmail.com>
2026-08-06 23:32:02 +0600

    fix whitenoise static files storage for vercel

--- build_files.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Installing requirements..."
-python3 -m pip install -r requirements.txt
-
-echo "Collecting static files..."
-python3 manage.py collectstatic --noinput --clear
-
-echo "Build process completed!"
