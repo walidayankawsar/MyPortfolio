07fcc4d69115bb99ae0a776f439f919ff5672289
walidayankawsar <walidayankawsar@gmail.com>
2026-08-06 16:12:58 +0600

    staticfiles storage added

--- Portfolio/settings.py
+++ Portfolio/settings.py
@@ -127,7 +127,7 @@ STORAGES = {
         "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
     },
     "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
     },
 }
 
